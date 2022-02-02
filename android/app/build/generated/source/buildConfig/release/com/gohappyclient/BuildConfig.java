6efcfdca442800d94f61e6c42aadd676ae7b2f3d
Rakshit Sharma <rashu.sharma14@gmail.com>
2022-02-02 22:26:57 +0530

    logo added

--- android/app/build/generated/source/buildConfig/release/com/gohappyclient/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/gohappyclient/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.gohappyclient";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 9;
-  public static final String VERSION_NAME = "9.0";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "10.0";
 }
