7c2f48aa1c9ae88f2cbfdcf4d53108461e57e1d1
Oleksandra Nahorna <72212159+onahorna@users.noreply.github.com>
2023-02-11 09:59:43 +0200

    Alarms & Notification permission asking

--- android/app/src/main/java/com/gohappyclient/MainActivity.java
+++ android/app/src/main/java/com/gohappyclient/MainActivity.java
@@ -1,20 +1,60 @@
 package com.gohappyclient;
 
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-
+import android.provider.Settings;
 public class MainActivity extends ReactActivity {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "GoHappyClient";
-  }
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+@Override
+protected String getMainComponentName() {
+return "GoHappyClient";
 }
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(null);
+if (!checkAlarmsRemindersPermission()) { // && checkAlarmsRemindersPermission()
+
+//!checkNotificationPermission() && 
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+showNotificationPermissionDialog();
+}
+}
+}
+// private boolean checkNotificationPermission() {
+// String notificationAccessEnabled = Settings.Secure.getString(
+// getContentResolver(),
+// "enabled_notification_listeners"
+// );
+// return notificationAccessEnabled != null && notificationAccessEnabled.contains(getPackageName());
+// }
+
+
+private boolean checkAlarmsRemindersPermission() {
+String alarmRemindersEnabled = Settings.Secure.getString(
+getContentResolver(),
+"enabled_alarm_reminders"
+);
+return alarmRemindersEnabled != null && alarmRemindersEnabled.contains(getPackageName());
+}
+
+private void showNotificationPermissionDialog() {
+AlertDialog.Builder builder = new AlertDialog.Builder(this);
+builder.setTitle("Allow Alarms & Reminders");
+builder.setMessage("Allow GoHHappyClub to remind you of ongoing events?");
+builder.setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+@Override
+public void onClick(DialogInterface dialog, int which) {
+Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+startActivity(intent);
+}
+});
+builder.create().show();
+}
+}
+
+
