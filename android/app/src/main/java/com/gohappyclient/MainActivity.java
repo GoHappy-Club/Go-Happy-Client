e4796291472cabe7ba2128963575f50671581196
onahorna <onahorna@uwaterloo.ca>
2023-02-13 18:07:43 -0500

    Resolved an issue with a check alarms&reminders. Waiting for check from teammates.

--- android/app/src/main/java/com/gohappyclient/MainActivity.java
+++ android/app/src/main/java/com/gohappyclient/MainActivity.java
@@ -1,60 +1,53 @@
 package com.gohappyclient;
 
-
-import android.app.AlertDialog;
+import android.app.AlarmManager;
 import android.content.Context;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import com.facebook.react.ReactActivity;
 import android.provider.Settings;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import com.facebook.react.ReactActivity;
+
 public class MainActivity extends ReactActivity {
-@Override
-protected String getMainComponentName() {
-return "GoHappyClient";
-}
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(null);
-if (!checkAlarmsRemindersPermission()) { // && checkAlarmsRemindersPermission()
+    @Override
+    protected String getMainComponentName() {
+        return "GoHappyClient";
+    }
 
-//!checkNotificationPermission() && 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-showNotificationPermissionDialog();
-}
-}
-}
-// private boolean checkNotificationPermission() {
-// String notificationAccessEnabled = Settings.Secure.getString(
-// getContentResolver(),
-// "enabled_notification_listeners"
-// );
-// return notificationAccessEnabled != null && notificationAccessEnabled.contains(getPackageName());
-// }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            checkAlarmsRemindersPermission();
+        }
+    }
 
-private boolean checkAlarmsRemindersPermission() {
-String alarmRemindersEnabled = Settings.Secure.getString(
-getContentResolver(),
-"enabled_alarm_reminders"
-);
-return alarmRemindersEnabled != null && alarmRemindersEnabled.contains(getPackageName());
-}
+    private boolean checkAlarmsRemindersPermission() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        boolean hasPermission = alarmManager.canScheduleExactAlarms();
+        if (!hasPermission) {
+            showNotificationPermissionDialog();
+        }
+        return hasPermission;
+    }
 
-private void showNotificationPermissionDialog() {
-AlertDialog.Builder builder = new AlertDialog.Builder(this);
-builder.setTitle("Allow Alarms & Reminders");
-builder.setMessage("Allow GoHHappyClub to remind you of ongoing events?");
-builder.setPositiveButton("Allow", new DialogInterface.OnClickListener() {
-@Override
-public void onClick(DialogInterface dialog, int which) {
-Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
-startActivity(intent);
+    private void showNotificationPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Allow Alarms & Reminders");
+        builder.setMessage("Allow GoHappyClub to remind you of ongoing events?");
+        builder.setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                startActivity(intent);
+            }
+        });
+        builder.create().show();
+    }
 }
-});
-builder.create().show();
-}
-}
-
-
