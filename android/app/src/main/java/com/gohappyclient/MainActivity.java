8f4657ccb182bce758fbfb522dbf0adad7a2a576
onahorna <onahorna@uwaterloo.ca>
2023-02-23 14:14:50 -0500

    Crashes on message received from Firebase.

--- android/app/src/main/java/com/gohappyclient/MainActivity.java
+++ android/app/src/main/java/com/gohappyclient/MainActivity.java
@@ -13,7 +13,24 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import com.facebook.react.ReactActivity;
 
+//import com.google.firebase.iid.FirebaseInstanceId;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import androidx.annotation.RequiresApi;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
+//import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends ReactActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int NOTIFICATION_REQUEST_CODE = 1234;
+
     @Override
     protected String getMainComponentName() {
         return "GoHappyClient";
@@ -26,6 +43,31 @@ public class MainActivity extends ReactActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             checkAlarmsRemindersPermission();
         }
+//        FirebaseMessaging.getInstance().getToken()
+//                .addOnCompleteListener(new OnCompleteListener<String>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<String> task) {
+//                        if (!task.isSuccessful()) {
+//                            //System.out.println("Fetching FCM registration token failed");
+//
+//                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+//                            return;
+//                        }
+//
+//                        // Get new FCM registration token
+//                        String token = task.getResult();
+//
+//                        // Log and toast
+//                        //System.out.println(token);
+//                        //Toast.makeText(MainActivity.this, token, Toast.LENGTH_LONG).show();
+//                        //etToken.setText(token);
+//                        String msg = token;
+//                        Log.d(TAG, msg);
+//                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+//                        //console.log(token);
+//                        //Log.d(TAG, msg.getText());
+//                    }
+//                });
     }
 
     private boolean checkAlarmsRemindersPermission() {
@@ -49,5 +91,40 @@ public class MainActivity extends ReactActivity {
             }
         });
         builder.create().show();
+    }
+
+
+    public void runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        // [END fcm_runtime_enable_auto_init]
+    }
+
+    private void logRegToken() {
+        // [START log_reg_token]
+         FirebaseMessaging.getInstance().getToken()
+                 .addOnCompleteListener(new OnCompleteListener<String>() {
+                     @Override
+                     public void onComplete(@NonNull Task<String> task) {
+                         if (!task.isSuccessful()) {
+                             //System.out.println("Fetching FCM registration token failed");
+                            
+                             Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                             return;
+                         }
+
+                         // Get new FCM registration token
+                         String token = task.getResult();
+
+                         // Log and toast
+                         //System.out.println(token);
+                        // Toast.makeText(MainActivity.this, "Your device token" + token, Toast.LENGTH_SHORT).show();
+                         //etToken.setText(token);
+                          String msg = "FCM Registration token: " + token;
+                          Log.d(TAG, msg);
+                         //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                     }
+                 });
+        // [END log_reg_token]
     }
 }
