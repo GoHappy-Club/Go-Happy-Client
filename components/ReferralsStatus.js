4fdd9cc65231722ecb7ee67d0e888fbb222c83ba
szgabrielchen <szgabrielchen@gmail.com>
2023-03-25 15:46:22 -0400

    several progress bars

--- components/ReferralsStatus.js
+++ components/ReferralsStatus.js
@@ -20,43 +20,52 @@ export default class PBA extends React.Component {
   render() {
     //this.referralProgressNumberToPercentage.bind(this);
     const barWidth = Dimensions.get('screen').width*0.5;
-    const progressCustomStyles = {
-      backgroundColor: 'black', 
-      borderRadius: 10,
-      borderColor: 'black',
-      flex: 1,
-    };
+    const openedChestCount = Math.floor(this.props.numberReferrals/7);
+    const currentCount = this.props.numberReferrals - (openedChestCount*7);
     // var [chestOpened, setChestOpened] = useState(false);
     var chestType = (
-      this.props.numberReferrals < this.state.referralComplete
+      currentCount < this.state.referralComplete
       )
       ? require('../images/chest-closed.png')
       : require('../images/chest-opened.png');
     var chest = <Image 
       // resizeMode="cover"
-      style={{
-        width: "25%",
-        height: "25%",
-        aspectRatio: 1/1,
-        marginLeft: "5%",
-        alignSelf: "center",
-        // marginRight: "10%",
-      }}
+      style={styles.chest}
       source={chestType} />;
+    var chestOpened = <Image
+      // resizeMode="cover"
+      style={styles.chest}
+      source={require('../images/chest-opened.png')} />;    
+    // getting number of rounds
+
+    const chestComponents = [];
+    for (let i=0; i < openedChestCount; i++){
+      chestComponents.push(
+        <View style={{flexDirection: 'row', marginBottom: 10}}>
+          <Progress.Bar 
+            style={{borderRadius: 30,}}
+            //animated={false}
+            color="black" borderColor="black"
+            progress={1.0} width={barWidth} height={29} />
+          {chestOpened}
+          <Text style={styles.label2}>7/7</Text>
+        </View>
+      );
+    }
  
     return (
       <View style={styles.container}>
-        <View>
-            <View style={{flexDirection: 'row'}}>
-              <View>
-                <Text style={styles.label}>Referred and Attended:</Text>
-                <Text style={styles.label1}>{this.props.numberReferrals} / 7</Text>
-                <Progress.Bar 
-                  color="black" borderColor="black"
-                  progress={this.props.numberReferrals/7} width={barWidth} />
-              </View>
-              {chest}
-            </View>
+        <View> 
+          <Text style={styles.label}>Referral Quest {"\n"} ({this.props.numberReferrals} completed)</Text>
+          {chestComponents}  
+          <View style={{flexDirection: 'row'}}>
+            <Progress.Bar 
+              style={{borderRadius: 30,}}
+              color="black" borderColor="black"
+              progress={currentCount/7} width={barWidth} height={29} />
+            {chest}
+            <Text style={styles.label2}>{currentCount}/7</Text>
+          </View>
         </View>
       </View>
     );
@@ -84,4 +93,18 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     alignSelf: "center",
   },
+  label2: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 20,
+    alignSelf: "center",
+  },
+  chest: {
+    width: 29,
+    height: 29,
+    aspectRatio: 1/1,
+    marginLeft: "5%",
+    alignSelf: "center",
+  }
 });
