9c3089f6e852df6e43c43bacb0a379907f02b0df
mehul-gohappy <mehul.sharma@gohappyclub.in>
2024-09-03 16:49:21 +0530

    remove whatsapp groups for age<50

--- components/Profile.js
+++ components/Profile.js
@@ -412,22 +412,24 @@ class Profile extends Component {
               </View>
             </TouchableOpacity>
           </View>
-          <View style={{ width: Dimensions.get("window").width * 0.9 }}>
-            <TouchableOpacity
-              style={{
-                width: "100%",
-                borderTopWidth: 1,
-                borderColor: "#E0E0E0",
-              }}
-              onPress={this.openWhatsApp}
-            >
-              <View>
-                <Text style={styles.optionList}>
-                  Join Whatsapp Group
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+          {profile.age > 50 && (
+            <View style={{ width: Dimensions.get("window").width * 0.9 }}>
+              <TouchableOpacity
+                style={{
+                  width: "100%",
+                  borderTopWidth: 1,
+                  borderColor: "#E0E0E0",
+                }}
+                onPress={this.openWhatsApp}
+              >
+                <View>
+                  <Text style={styles.optionList}>
+                    Join Whatsapp Support Group
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
           <View style={{ width: Dimensions.get("window").width * 0.9 }}>
             <TouchableOpacity
               style={{
@@ -474,13 +476,15 @@ class Profile extends Component {
 					</View> */}
         </ScrollView>
 
-        <FAB
-          style={styles.fab}
-          icon={({ size, color }) => (
-            <FontAwesomeIcon icon={faComment} color={"white"} size={25} />
-          )}
-          onPress={this.openWhatsApp}
-        />
+        {profile.age > 50 && (
+          <FAB
+            style={styles.fab}
+            icon={({ size, color }) => (
+              <FontAwesomeIcon icon={faComment} color={"white"} size={25} />
+            )}
+            onPress={this.openWhatsApp}
+          />
+        )}
       </View>
     );
   }
