32d3eaf4e5688a050ecc54e0baa0c25d1f6b30b0
mehul1101 <mehul@drupsc.com>
2024-10-07 17:35:56 +0530

    Header component ready

--- screens/myProfileScreen/MyProfileScreen.js
+++ screens/myProfileScreen/MyProfileScreen.js
@@ -1,109 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-
-// import AsyncStorage from '@react-native-community/async-storage';
-// import { Container, Header, Content, Left, Body, Right, Icon, Title, Form, Item, Input, Label } from 'native-base';
 import { MaterialIndicator } from "react-native-indicators";
 import MyProfile from "../../components/Profile";
 import { Colors } from "../../assets/colors/color";
 
-export default class MyProfileScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      phoneNumber: "",
-      password: "",
-      showAlert: false,
-      loader: false,
-    };
-  }
-  render() {
-    if (this.state.loader == true) {
-      // return (<ActivityIndicator size='large' color="#0A1045" style={{flex: 1,justifyContent: "center",flexDirection: "row",justifyContent: "space-around",padding: 10}}/>);
-      return (
-        <MaterialIndicator
-          color={Colors.white}
-          style={{ backgroundColor: Colors.materialIndicatorColor }}
-        />
-      );
-    }
-    const navigation = this.props.navigation;
-    const title = "Login";
-    return <MyProfile navigation={this.props.navigation} />;
-  }
-}
+const MyProfileScreen = () => {
+  const [loader, setLoader] = useState(false);
 
-const styles = StyleSheet.create({
-  container1: {
-    flex: 1,
-    backgroundColor: Colors.materialIndicatorColor,
-  },
-  input: {
-    width: "90%",
-    backgroundColor: Colors.white,
-    padding: 15,
-    marginBottom: 10,
-  },
-  btnContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  userBtn: {
-    backgroundColor: "#f0ad4e",
-    paddingVertical: 15,
-    height: 60,
-  },
-  btnTxt: {
-    fontSize: 20,
-    textAlign: "center",
-    color: Colors.black,
-    fontWeight: "700",
-  },
-  registerTxt: {
-    marginTop: 5,
-    fontSize: 15,
-    textAlign: "center",
-    color: Colors.white,
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: "center",
-    margin: 10,
-    color: Colors.white,
-  },
-  logo: {
-    width: 150,
-    height: 150,
-  },
-  logoContainer: {
-    alignItems: "center",
-    flexGrow: 1,
-    justifyContent: "center",
-  },
-  formContainer: {},
-  title: {
-    color: Colors.white,
-    marginTop: 10,
-    width: 160,
-    opacity: 0.9,
-    textAlign: "center",
-  },
-  newinput: {
-    height: 50,
-    backgroundColor: "rgba(255,255,255,0.2)",
-    marginBottom: 10,
-    color: Colors.white,
-    paddingHorizontal: 10,
-  },
-  container2: {
-    padding: 25,
-  },
-  title2: {
-    color: Colors.white,
-    marginTop: "30%",
-    marginBottom: 10,
-    opacity: 0.9,
-    textAlign: "center",
-    fontSize: 30,
-  },
-});
+  if (loader) {
+    return (
+      <MaterialIndicator
+        color={Colors.white}
+        style={{ backgroundColor: Colors.materialIndicatorColor }}
+      />
+    );
+  }
+
+  return <MyProfile />;
+};
+
+export default MyProfileScreen;
+
+const styles = StyleSheet.create({});
