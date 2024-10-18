b820cf4f521825ed32f339a0d57dd986a16bedeb
mehul1101 <mehul@drupsc.com>
2024-10-18 09:29:44 +0530

    dummy api calls plus storing membership with fcm done

--- config/CustomAxios.js
+++ config/CustomAxios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { JWT_TOKEN } from "@env";
 const customAxios = axios.create();
+
 if (JWT_TOKEN && JWT_TOKEN.length > 0) {
   customAxios.interceptors.request.use(
     (config) => {
