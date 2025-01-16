cbadffa4df254d4f870216d3e90b8d7c8d0a9135
mehul1101 <mehul@drupsc.com>
2025-01-16 22:59:38 +0530

    alert for giving the hash

--- screens/loginScreen/LoginScreen.js
+++ screens/loginScreen/LoginScreen.js
@@ -11,6 +11,7 @@ import {
   KeyboardAvoidingView,
   Keyboard,
   SafeAreaView,
+  Alert,
 } from "react-native";
 //import axios from "axios";
 import AwesomeAlert from "react-native-awesome-alerts";
@@ -87,6 +88,8 @@ class LoginScreen extends Component {
         }
         this.setState({ referralCode: url.url.split("=")[1] });
       });
+
+      RNOtpVerify.getHash().then((hash)=>Alert.alert("Hash is",hash[0]))
 
     RNOtpVerify.getOtp()
       .then((p) => {
