20462b00189bfb4a8bf2a5fcada1f61c1d7232d8
Rakshit Sharma <r22sharm>
2025-01-16 22:36:47 -0500

    loader position fix

--- commonComponents/GOHLoader.js
+++ commonComponents/GOHLoader.js
@@ -20,17 +20,17 @@ export default GOHLoader;
 
 const styles = StyleSheet.create({
   loaderAnim: {
-    position: "absolute",
-    top: 0,
+    // position: "absolute",
+    // top: 0,
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    left: 0,
-    right: 0,
-    bottom: 0,
+    // left: 0,
+    // right: 0,
+    // bottom: 0,
     opacity: 1,
-    height:hp(100),
-    width:wp(100)
+    height: hp(100),
+    width: wp(100),
   },
 });
