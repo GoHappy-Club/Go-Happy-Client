24a552f08428f6ca312558f1d880fbf2aee4e7a9
mehul1101 <mehul@drupsc.com>
2025-01-27 07:05:40 +0530

    Enhance Paytring integration with loading states and improved back navigation handling

--- commonComponents/PaytringView.js
+++ commonComponents/PaytringView.js
@@ -1,5 +1,5 @@
 import { BackHandler, StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Paytring from "react-native-paytring";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
@@ -7,15 +7,43 @@ const PaytringView = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { callback, error_handler, order_id } = route?.params;
+
+  const [webViewOpen, setWebViewOpen] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (webViewOpen) {
+        console.log("here");
+        setWebViewOpen(false);
+        return true;
+      } else {
+        console.log("here1");
+        setWebViewOpen(false);
+        navigation.goBack();
+        return true;
+      }
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, [webViewOpen]);
+
   return (
     <Paytring
       init={(args) => {
+        setWebViewOpen(true);
         args.open(order_id);
       }}
       success={() => {
+        setWebViewOpen(false);
         callback();
       }}
       failure={() => {
+        setWebViewOpen(false);
         error_handler();
       }}
     />
