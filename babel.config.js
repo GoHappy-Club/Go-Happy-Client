efbfe62063efbe335922512a15f361983de20041
mehul1101 <mehul@drupsc.com>
2025-01-29 07:27:40 +0530

    remove dotenv from babel

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   presets: ["babel-preset-expo"],
-  plugins: [['module:react-native-dotenv', {
-    "moduleName": "@env",
-    "path": ".env",
-    "blocklist": null,
-    "allowlist": null,
-    "safe": false,
-    "allowUndefined": true
-  }],
-'react-native-reanimated/plugin']
+  plugins: ["react-native-reanimated/plugin"],
 };
