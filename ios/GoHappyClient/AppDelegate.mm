701cae43bd07f4e531e5c48e182408bf3e3662ed
Rakshit Sharma <r22sharm>
2025-01-29 21:29:10 -0500

    Revert "updating ios code to support deep links"
    This reverts commit 59e3df526504bd0c321d771d18ec1b037e237abf.

--- ios/GoHappyClient/AppDelegate.mm
+++ ios/GoHappyClient/AppDelegate.mm
@@ -2,7 +2,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTLinkingManager.h>
+
 
 @implementation AppDelegate
 
@@ -28,7 +28,7 @@
   [userInfo setObject:options forKey:@"options"];
   [userInfo setObject:url forKey:@"openUrl"];
   [[NSNotificationCenter defaultCenter] postNotificationName: @"ApplicationOpenURLNotification" object:nil userInfo:userInfo];
-  return [RCTLinkingManager application:app openURL:url options:options];
+  return YES;
 }
 
 - (NSURL *)bundleURL
@@ -40,11 +40,4 @@
 #endif
 }
 
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
- restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
-{
- return [RCTLinkingManager application:application
-                  continueUserActivity:userActivity
-                    restorationHandler:restorationHandler];
-}
 @end
