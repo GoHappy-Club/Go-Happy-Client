708d2fb3e1ce53936d5618ea11c2f5ea46c2ba73
mehul1101 <mehul@drupsc.com>
2025-02-11 09:13:14 +0530

    Refactor OTP handling in LoginScreen and update OTPAuthWrapper to use new API endpoints

--- services/Authentication/OTPAuthWrapper.js
+++ services/Authentication/OTPAuthWrapper.js
@@ -1,44 +1,22 @@
-import axiosOg from "axios";
-import Config from "react-native-config";
 import firebase from "@react-native-firebase/app";
 import "@react-native-firebase/auth";
+import axios from "../../config/CustomAxios";
 
 const GUPSHUP_BASE_URL = "https://enterprise.smsgupshup.com/GatewayAPI/rest";
 
 const OTPAuthWrapper = {
   GupshupWrapper: {
     sendOtp: (phoneNumber) => {
-      const messageTemplate =
-        "Dear User,\n\n" +
-        "Your OTP for login to GoHappy Club is %code%. This code is valid for 30 minutes. Please do not share this OTP.\n\n" +
-        "Regards,\nGoHappy Club Team";
-
-      return axiosOg.get(GUPSHUP_BASE_URL, {
-        params: {
-          userid: Config.GS_USER_ID,
-          password: Config.GS_PASSWORD,
-          method: "TWO_FACTOR_AUTH",
-          v: "1.1",
-          phone_no: phoneNumber,
-          format: "text",
-          otpCodeLength: "6",
-          otpCodeType: "NUMERIC",
-          msg: messageTemplate,
-        },
+      return axios.post(`${SERVER_URL}/auth/init`, {
+        phone: phoneNumber,
       });
     },
 
     verifyOtp: async (phoneNumber, code) => {
       // Implementation for OTP verification
-      return axiosOg.get(GUPSHUP_BASE_URL, {
-        params: {
-          userid: Config.GS_USER_ID,
-          password: Config.GS_PASSWORD,
-          method: "TWO_FACTOR_AUTH",
-          v: "1.1",
-          phone_no: phoneNumber,
-          otp_code: code,
-        },
+      return axios.post(`${SERVER_URL}/auth/verify`, {
+        phone: phoneNumber,
+        otp: code,
       });
     },
   },
