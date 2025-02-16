93ccc6f07e45d80ce89768af17442c2868ef8cdf
mehul1101 <mehul@drupsc.com>
2025-02-16 22:51:17 +0530

    Add cspell and auto sort imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,58 @@
-export const root = true;
-export const env = {
-  node: true,
-};
-export const extendConfigs = ["@react-native-community", "eslint:recommended"];
-export const plugins = ["react", "react-native", "unused-imports"];
-export const settings = {
-  react: {
-    version: "detect",
+export default {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
   },
-};
-export const rules = {
-  "prettier/prettier": [
-    "warn",
-    {
-      singleQuote: true,
-      semi: true,
+  extends: ["@react-native-community", "eslint:recommended"],
+
+  plugins: ["react", "react-native", "unused-imports", "simple-import-sort"],
+
+  settings: {
+    react: {
+      version: "detect",
     },
-  ],
-  "no-unused-vars": "error",
-  "unused-imports/no-unused-imports": "error",
-  "no-console": "warn",
-  "sort-imports": [
-    "error",
-    {
-      ignoreDeclarationSort: true,
+  },
+
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+    "no-unused-vars": "error",
+    "unused-imports/no-unused-imports": "error",
+    "no-console": "warn",
+
+    "sort-imports": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+
+    "max-len": [
+      "error",
+      {
+        code: 70,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+  },
+
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-  ],
-};
-export const globals = {
-  SERVER_URL: "readonly",
-  crashlytics: "readonly",
-  axios: "readonly",
-  AsyncStorage: "readonly",
+  },
+
+  globals: {
+    globalThis: "readonly",
+    SERVER_URL: "readonly",
+    crashlytics: "readonly",
+    axios: "readonly",
+    AsyncStorage: "readonly",
+  },
 };
