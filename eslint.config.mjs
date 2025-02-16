2dc8a66f2f1905d5f22c119d93314dda6250b0fe
mehul1101 <mehul@drupsc.com>
2025-02-16 22:51:17 +0530

    Add cspell and auto sort imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+
+  {
+    plugins: {
+      "simple-import-sort": pluginImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
